2cd2b11c5bd4f848ecbc0e576ed4ef781fa12c92
Rituraj Jha <riturajreal01@gmail.com>
2024-06-03 08:21:06 +0530

    added some ui components and added theme from next-themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
@@ -5,7 +6,7 @@ export default function Home() {
    flex justify-center items-center flex-col
    overflow-hidden sm:px-10 px-5 ">
       <div className="max-w-7xl w-full">
-        <h1>Hello Portfolio</h1>
+        <Hero/>
       </div>
    </main>
   );
