0c1da1383f7c82e939e8f069c46b45f4112c8e70
Rituraj Jha <riturajreal01@gmail.com>
2024-06-08 09:24:59 +0530

    fixed footer

--- components/Footer..tsx
+++ components/Footer..tsx
@@ -6,15 +6,6 @@ import MagicButton from "./ui/MagicButton";
 const Footer = () => {
   return (
     <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
       <div className="flex flex-col items-center justify-between">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
